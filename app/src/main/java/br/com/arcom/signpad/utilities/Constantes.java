69dadae6aaf7ede074cb821d2b2d1c2140aadb45
lucas.silva <lucas.silva@arcom.com.br>
2021-05-03 11:04:56 -0300

    dev = false

--- app/src/main/java/br/com/arcom/signpad/utilities/Constantes.java
+++ app/src/main/java/br/com/arcom/signpad/utilities/Constantes.java
@@ -5,6 +5,6 @@ public interface Constantes {
     String TAG_LOG_SIGNPAD = "tagLogSignpad";
     String DATABASE_NAME = "signpad_database.db";
     int DATABASE_VERSION = 1;
-    Boolean DEV = true;
+    Boolean DEV = false;
 
 }
